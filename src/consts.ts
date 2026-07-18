b04f466b945545315c136ffc895f9b72291a317e
Lumirui <3831983696@qq.com>
2026-07-18 23:39:54 +0800

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -11,6 +11,6 @@ export const GITHUB_CONFIG = {
 	OWNER: process.env.NEXT_PUBLIC_GITHUB_OWNER || 'Lumirui',
 	REPO: process.env.NEXT_PUBLIC_GITHUB_REPO || 'Lumiruiblog',
 	BRANCH: process.env.NEXT_PUBLIC_GITHUB_BRANCH || 'main',
-	APP_ID: process.env.NEXT_PUBLIC_GITHUB_APP_ID || '-',
+	APP_ID: process.env.NEXT_PUBLIC_GITHUB_APP_ID || 'https://lumiruiblog.vercel.app/',
 	ENCRYPT_KEY: process.env.NEXT_PUBLIC_GITHUB_ENCRYPT_KEY || 'wudishiduomejimo',
 } as const
